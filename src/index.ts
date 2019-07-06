ed59ae2519ed2e0948cc0d0d5c64e676e7b1d198
Иван Митирев <arti1793@arti1793-MacBook-Pro.local>
2019-07-06 22:13:33 +0300

    added proxy agant and started bot

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,13 @@
 import dotenv from 'dotenv';
-import { SocksClient, SocksClientOptions } from 'socks';
 import { setupBot } from './setupBot';
+import proxyAgent from 'proxy-agent';
 
 dotenv.config();
 
-const socksClientOptions: SocksClientOptions = {
-    proxy: {
-        host: process.env.PROXY_HOST,
-        port: Number(process.env.PROXY_PORT),
-        type: 5,
-        userId: process.env.PROXY_LOGIN,
-        password: process.env.PROXY_PSSWD,
-    },
-
-    command: 'connect',
-
-    destination: {
-        host: 'api.telegram.org',
-        port: 80,
-    },
-};
-(async () => {
-    try {
-        console.log(socksClientOptions);
-        const info = await SocksClient.createConnection(socksClientOptions);
-        setupBot(info);
-    } catch (error) {
-        throw new Error(error);
-    }
-})();
+setupBot(
+    new proxyAgent(
+        `${process.env.PROXY_PROTOCOL}://${process.env.PROXY_LOGIN}:${
+            process.env.PROXY_PSSWD
+        }@${process.env.PROXY_HOST}:${process.env.PROXY_PORT}`
+    )
+);
--- src/setupBot.ts
+++ src/setupBot.ts
@@ -1,10 +1,8 @@
 import Telegraf from 'telegraf';
-import { SocksClientEstablishedEvent } from 'socks/typings/common/constants';
-
-export function setupBot(socksClient: SocksClientEstablishedEvent) {
-    console.log(socksClient);
+import { Agent } from 'https';
+export function setupBot(agent: Agent) {
     const bot = new Telegraf(process.env.BOT_TOKEN, {
-        // telegram: { agent: socksClient },
+        telegram: { agent: agent },
     });
     bot.start(ctx => ctx.reply('Welcome!'));
     bot.help(ctx => ctx.reply('Send me a sticker'));
