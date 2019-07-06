566b7f6fad588f65fe36d3f51bd203a9ce1b2ce0
Иван Митирев <arti1793@arti1793-MacBook-Pro.local>
2019-07-06 22:36:44 +0300

    added axios added request boilerplate

--- src/setupBot.ts
+++ src/setupBot.ts
@@ -1,12 +1,20 @@
 import Telegraf from 'telegraf';
 import { Agent } from 'https';
+import axios from 'axios';
+
 export function setupBot(agent: Agent) {
     const bot = new Telegraf(process.env.BOT_TOKEN, {
         telegram: { agent: agent },
     });
-    bot.start(ctx => ctx.reply('Welcome!'));
-    bot.help(ctx => ctx.reply('Send me a sticker'));
-    bot.on('sticker', ctx => ctx.reply('👍'));
-    bot.hears('hi', ctx => ctx.reply('Hey there'));
+    bot.start(ctx => ctx.reply('Welcome! send me a jira worksheet file.'));
+    bot.on('document', async ctx => {
+        const { file } = await axios.post(
+            `${process.env.REPORT_REST_SERVICE}/api/report`,
+            {
+                data: ctx.message.document,
+            }
+        );
+        return ctx.telegram.sendDocument(ctx.from.id, file);
+    });
     bot.launch();
 }
